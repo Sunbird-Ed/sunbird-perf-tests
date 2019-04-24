1d96a2fcdd73522d223fb69d46df27b5b7b429f7
rajat <rajatgupta0889@gmail.com>
2019-04-24 17:04:47 +0530

    Issue add root-org script

---
+++ test-data-prepare/root-org/root-org.sh
@@ -0,0 +1,27 @@
+start(){
+	echo $rootOrgSize
+	local i=1
+	while [ $i -le $rootOrgSize ] 
+	do 
+    createRootOrgCql $i
+    i=`expr $i + 1`
+	done 
+}
+
+createRootOrgCql(){
+	now=`date +%Y-%m-%d.%H:%M:%S%z`
+	# echo $now
+	local query="Insert into sunbird.organisation (id, orgName, channel, createddate, isrootorg,slug,status, hashtagid) values ('root-org-$1','root-org-name-$1','channel$1','$now',true,'channel$1',1,'channel$1');"
+	echo $query
+	# echo $rootOrgSize
+	if [ $1 == 1 ]
+	then
+		echo $query > $orgfile
+	else
+		echo $query >> $orgfile
+	fi
+}
+
+rootOrgSize="$1"
+orgfile="$2"
+start
