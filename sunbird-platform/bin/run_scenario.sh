51f50867410cacf3b00ccb52139a8618dba4740c
Vinaya Kumar B <vinayab@ilimi.in>
2019-04-26 06:06:59 +0000

    Logs for page-assemble API

--- sunbird-platform/bin/run_scenario.sh
+++ sunbird-platform/bin/run_scenario.sh
@@ -11,7 +11,8 @@ apiKey=$8
 accessToken=$9
 csvFile=${10}
 
-JMETER_HOME=~/apache-jmeter-5.1.1
+# JMETER_HOME=~/apache-jmeter-5.1.1
+JMETER_HOME=/mnt/data/benchmark/apache-jmeter-4.0
 SCENARIO_LOGS=~/sunbird-perf-tests/sunbird-platform/logs/$scenario_name
 JMETER_CLUSTER_IPS=$ips
 
@@ -43,18 +44,33 @@ echo "apiKey = " ${apiKey}
 echo "accessToken = " ${accessToken}
 echo "csvFile = " ${csvFile}
 
-sed -i "s/THREADS_COUNT/${numThreads}/g" $JMX_FILE_PATH
-sed -i "s/RAMPUP_TIME/${rampupTime}/g" $JMX_FILE_PATH
-sed -i "s/CTRL_LOOPS/${ctrlLoops}/g" $JMX_FILE_PATH
-sed -i "s/ACCESS_TOKEN/${accessToken}/g" $JMX_FILE_PATH
-sed -i "s/HOST/${host}/g" $JMX_FILE_PATH
-sed -i "s/API_KEY/${apiKey}/g" $JMX_FILE_PATH
-sed -i "s#CSV_FILE#${csvFile}#g" $JMX_FILE_PATH
+sed -i "s/THREADS_COUNT/${numThreads}/g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
 
-cat $JMX_FILE_PATH > script.jmx
+sed -i "s/RAMPUP_TIME/${rampupTime}/g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
 
-echo "nohup $JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R28.0.0.24,28.0.0.25 -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log"
-nohup $JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R28.0.0.24,28.0.0.25 -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log 2>&1 &
-#nohup $JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R28.0.0.24,28.0.0.25 -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log 2>&1 &
+sed -i "s/CTRL_LOOPS/${ctrlLoops}/g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
+
+sed -i "s/ACCESS_TOKEN/${accessToken}/g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
+
+sed -i "s/HOST/${host}/g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
+
+sed -i "s/API_KEY/${apiKey}/g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
+
+sed -i "s#CSV_FILE#${csvFile}#g" $JMX_FILE_PATH > jmx.tmp
+mv jmx.tmp $JMX_FILE_PATH
+
+echo "Running ... "
+echo "$JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R ${ips} -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log"
+
+nohup $JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R ${ips} -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log 2>&1 &
+
+echo "Log file ..."
+echo "$SCENARIO_LOGS/$scenario_id/logs/scenario.log"
 
 echo "Execution of $scenario_id Complete."
