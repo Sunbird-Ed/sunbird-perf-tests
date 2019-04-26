aa43ff39fc2b25ae241f7905ae319785eb80955f
rajat <rajatgupta0889@gmail.com>
2019-04-26 19:13:06 +0530

    Issue update realm

--- sunbird-platform/test-data-preparation/sub-org-user/user-password-update.sh
+++ sunbird-platform/test-data-preparation/sub-org-user/user-password-update.sh
@@ -22,13 +22,13 @@ start(){
 }
 updatePassword(){
 echo curl -X POST \
-      $baseurl/auth/admin/realms/master/users/user-$1-$2-$3/reset-password \
+      $baseurl/auth/admin/realms/sunbird/users/user-$1-$2-$3/reset-password \
       -H \''Authorization: Bearer '$token''\' \
       -H \''Content-Type: application/json'\' \
       -d \''{"type": "password", "value": "password", "temporary": "false"}'\' '| jq'
 
 	 curl -X POST \
-      $baseurl/auth/admin/realms/master/users/user-$1-$2-$3/reset-password \
+      $baseurl/auth/admin/realms/sunbird/users/user-$1-$2-$3/reset-password \
       -H 'Authorization: Bearer '$token'' \
       -H 'Content-Type: application/json' \
       -d '{"type": "password", "value": "password", "temporary": "false"}' | jq
