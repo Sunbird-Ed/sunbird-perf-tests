533bee58c01e1e638375561abe9b17eb6ec93f52
rajat <rajatgupta0889@gmail.com>
2019-04-26 18:44:52 +0530

    Issue change create suborg

--- sunbird-platform/test-data-preparation/sub-org-user/create-user-sub-org.sh
+++ sunbird-platform/test-data-preparation/sub-org-user/create-user-sub-org.sh
@@ -22,43 +22,35 @@ start(){
 }
 
 createUserForSubOrg(){
-	if [ $exec == true ]
-  	then  
-    echo  curl -X POST \
-      $baseurl/user/v2/create \
-      -H \''Content-Type: application/json'\' \
-      -d \' '{"request":{"firstName":"'name-$1-$2-$3'","email":"'user-$1-$2-$3@gmail.com'","emailVerified":true,"channel":"'channel$1'","organisationId":"'school-$1-$2'","password":"password"}}' \' '| jq' 
+	now=`date +%Y-%m-%d.%H:%M:%S%z`
+	local query="INSERT INTO sunbird.user (id, channel, email, emailverified , firstName, rootorgid,status,username,roles,createddate) VALUES ('user-$1-$2-$3','channel$1','user-$1-$2-$3@gmaol.com',true,'name-$1-$2-$3','root-org-$1',1,'username-$1-$2-$3',['PUBLIC'],'$now');"
+	echo $query
+	local user_root_org_query="INSERT INTO sunbird.user_org (id, organisationid,userid, hashtagid,orgjoindate,roles,isdeleted ) VALUES ('user-org-sub-root-$1','root-org-$1','user-$1-$2-$3','root-org-$1','$now',['PUBLIC'],false);"
+	local user_sub_org_query="INSERT INTO sunbird.user_org (id, organisationid,userid, hashtagid,orgjoindate,roles,isdeleted ) VALUES ('user-sub-org-$1','school-$1-$2','user-$1-$2-$3','school-$1-$2','$now',['PUBLIC'],false);"
 
-     curl -X POST \
-      $baseurl/user/v2/create \
+	echo curl -X POST \
+      $baseurl/auth/admin/realms/master/users/user-$1-$2-$3/reset-password \
+      -H \''Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJCX0d3V0RSWXMxZ0dDQzA2UGUxTnJFaGtJNzZQYVR3SUZvQUo1eEJZV19rIn0.eyJqdGkiOiIxMDE0ZmU5Yy1iN2QyLTQzYWYtYTkxMC00ZDk1OTQ5OWZhYzUiLCJleHAiOjE1NTYzMTk0NDksIm5iZiI6MCwiaWF0IjoxNTU2MjgzNDQ5LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXV0aC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiYWRtaW4tY2xpIiwic3ViIjoiMmIzNzI5NjYtZDI0Yy00YTRmLWE4ODYtNzBkMDc3YjdiOTRjIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWRtaW4tY2xpIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiNzZhNGM0MTktY2Q5NC00ZDkyLWE0YjMtNDVmN2U4MmFkMzkzIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6W10sInJlc291cmNlX2FjY2VzcyI6e30sIm5hbWUiOiJtYW56YXJ1bCBoYXF1ZSIsInByZWZlcnJlZF91c2VybmFtZSI6Im1hbnphcnVsIiwiZ2l2ZW5fbmFtZSI6Im1hbnphcnVsIiwiZmFtaWx5X25hbWUiOiJoYXF1ZSIsImVtYWlsIjoibWFuemFydWwuaGFxdWVAdGFyZW50by5jb20ifQ.IEjGTIF4AL3L-Oy3nj1lJvrmQ_pKiA9MSjYBXhEwcHxFgXtQhWZex_yLaiiQVRKjgfbw-gUS75zjtUFt58hFVKSW_brouNPADD-biq0qY-TRQAR_M3yB-zmKYaWtCIv3s1Rkg9-T2eIcl00guig0u-SDRi2wDXLbTbcB0zms1YLcXETklYvQEaoNxDxi6ncqbTMfXxzyuXEzqCKVjMWR8Qvc_QCXQ8d43_jnmInG7Ltwek0WQD0eTax0L9TlvqcfF3AVrLFcGcnERbkb5cqNO83M_sHHa7iy-kuJO68aWztIOp5LZvE8p7d1Gnj_DrR4AhjgiDkyUbH2uuS5jvbWNA'\' \
+      -H \''Content-Type: application/json'\' \
+      -d \''{"type": "password", "value": "password", "temporary": "false"}'\' '| jq'
+
+	 curl -X POST \
+      $baseurl/auth/admin/realms/master/users/user-$1-$2-$3/reset-password \
+      -H 'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJCX0d3V0RSWXMxZ0dDQzA2UGUxTnJFaGtJNzZQYVR3SUZvQUo1eEJZV19rIn0.eyJqdGkiOiIxMDE0ZmU5Yy1iN2QyLTQzYWYtYTkxMC00ZDk1OTQ5OWZhYzUiLCJleHAiOjE1NTYzMTk0NDksIm5iZiI6MCwiaWF0IjoxNTU2MjgzNDQ5LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXV0aC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiYWRtaW4tY2xpIiwic3ViIjoiMmIzNzI5NjYtZDI0Yy00YTRmLWE4ODYtNzBkMDc3YjdiOTRjIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWRtaW4tY2xpIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiNzZhNGM0MTktY2Q5NC00ZDkyLWE0YjMtNDVmN2U4MmFkMzkzIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6W10sInJlc291cmNlX2FjY2VzcyI6e30sIm5hbWUiOiJtYW56YXJ1bCBoYXF1ZSIsInByZWZlcnJlZF91c2VybmFtZSI6Im1hbnphcnVsIiwiZ2l2ZW5fbmFtZSI6Im1hbnphcnVsIiwiZmFtaWx5X25hbWUiOiJoYXF1ZSIsImVtYWlsIjoibWFuemFydWwuaGFxdWVAdGFyZW50by5jb20ifQ.IEjGTIF4AL3L-Oy3nj1lJvrmQ_pKiA9MSjYBXhEwcHxFgXtQhWZex_yLaiiQVRKjgfbw-gUS75zjtUFt58hFVKSW_brouNPADD-biq0qY-TRQAR_M3yB-zmKYaWtCIv3s1Rkg9-T2eIcl00guig0u-SDRi2wDXLbTbcB0zms1YLcXETklYvQEaoNxDxi6ncqbTMfXxzyuXEzqCKVjMWR8Qvc_QCXQ8d43_jnmInG7Ltwek0WQD0eTax0L9TlvqcfF3AVrLFcGcnERbkb5cqNO83M_sHHa7iy-kuJO68aWztIOp5LZvE8p7d1Gnj_DrR4AhjgiDkyUbH2uuS5jvbWNA' \
       -H 'Content-Type: application/json' \
-      -d '{"request":{"firstName":"'name-$1-$2-$3'","email":"'user-$1-$2-$3@gmail.com'","emailVerified":true,"channel":"'channel$1'","organisationId":"'school-$1-$2'","password":"password"}}' | jq
+      -d '{"type": "password", "value": "password", "temporary": "false"}' | jq
 
-  else
-    echo  curl -X POST \
-      $baseurl/user/v2/create \
-      -H \''Content-Type: application/json'\' \
-      -d \' '{"request":{"firstName":"'name-$1-$2-$3'","email":"'user-$1-$2-$3@gmail.com'","emailVerified":true,"channel":"'channel$1'","organisationId":"'school-$1-$2'","password":"password"}}' \' '| jq' 
-
-  fi 
-
-	# local query="INSERT INTO sunbird.user (id, channel, email, emailverified , firstName, rootorgid,status,username,roles,createddate) VALUES ('user-$1-$2-$3','channel$1','user-$1-$2-$3@gmaol.com',true,'name-$1-$2-$3','root-org-$1',1,'username-$1-$2-$3',['PUBLIC'],'$now');"
-	# echo $query
-	# local user_root_org_query="INSERT INTO sunbird.user_org (id, organisationid,userid, hashtagid,orgjoindate,roles,isdeleted ) VALUES ('user-org-sub-root-$1','root-org-$1','user-$1-$2-$3','root-org-$1','$now',['PUBLIC'],false);"
-	# local user_sub_org_query="INSERT INTO sunbird.user_org (id, organisationid,userid, hashtagid,orgjoindate,roles,isdeleted ) VALUES ('user-sub-org-$1','school-$1-$2','user-$1-$2-$3','school-$1-$2','$now',['PUBLIC'],false);"
-	
-	# echo $rootOrgSize
-	# if [ $count == 1 ]
-	# then
-	# 	count=`expr $count + 1`
-	# 	echo $query > $userfile
-	# 	echo $user_root_org_query > $userorgfile
-	# 	echo $user_sub_org_query >> $userorgfile
-	# else
-	# 	echo $user_root_org_query >> $userorgfile
-	# 	echo $user_sub_org_query >> $userorgfile
-	# 	echo $query >> $userfile	
-	# fi		
+	if [ $count == 1 ]
+	then
+		count=`expr $count + 1`
+		echo $query > $userfile
+		echo $user_root_org_query > $userorgfile
+		echo $user_sub_org_query >> $userorgfile
+	else
+		echo $user_root_org_query >> $userorgfile
+		echo $user_sub_org_query >> $userorgfile
+		echo $query >> $userfile	
+	fi		
 	 
 }
 count=1
@@ -66,5 +58,6 @@ rootOrgSize="$1"
 subOrgSize="$2"
 userSubOrgSize="$3"
 baseurl="$4"
-exec="$5"
+userfile="$5"
+userorgfile="$6"
 start
