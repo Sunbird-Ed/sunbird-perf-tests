df217c14aa69521d59433403fa64253ca943ceb3
Vinaya Kumar B <vinayab@ilimi.in>
2019-04-26 12:42:51 +0530

    Update scenario script and system settings jmx

--- sunbird-platform/bin/run_scenario.sh
+++ sunbird-platform/bin/run_scenario.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-ips=$1
-host=$2
-scenario_name=$3
-scenario_id=$4
-numThreads=$5
-rampupTime=$6
-ctrlLoops=$7
-apiKey=$8
-accessToken=$9
-csvFile=${10}
+jmeterHome=$1
+ips=$2
+host=$3
+scenario_name=$4
+scenario_id=$5
+numThreads=$6
+rampupTime=$7
+ctrlLoops=$8
+apiKey=$9
+accessToken=${10}
+csvFile=${11}
 
-# JMETER_HOME=~/apache-jmeter-5.1.1
 JMETER_HOME=/mnt/data/benchmark/apache-jmeter-4.0
+JMETER_HOME=${jmeterHome}
+
 SCENARIO_LOGS=~/sunbird-perf-tests/sunbird-platform/logs/$scenario_name
 JMETER_CLUSTER_IPS=$ips
 
@@ -44,19 +46,19 @@ echo "apiKey = " ${apiKey}
 echo "accessToken = " ${accessToken}
 echo "csvFile = " ${csvFile}
 
-sed -i "s/THREADS_COUNT/${numThreads}/g" $JMX_FILE_PATH > jmx.tmp
+sed "s/THREADS_COUNT/${numThreads}/g" $JMX_FILE_PATH > jmx.tmp
 mv jmx.tmp $JMX_FILE_PATH
 
-sed -i "s/RAMPUP_TIME/${rampupTime}/g" $JMX_FILE_PATH > jmx.tmp
+sed "s/RAMPUP_TIME/${rampupTime}/g" $JMX_FILE_PATH > jmx.tmp
 mv jmx.tmp $JMX_FILE_PATH
 
-sed -i "s/CTRL_LOOPS/${ctrlLoops}/g" $JMX_FILE_PATH > jmx.tmp
+sed "s/CTRL_LOOPS/${ctrlLoops}/g" $JMX_FILE_PATH > jmx.tmp
 mv jmx.tmp $JMX_FILE_PATH
 
-sed -i "s/ACCESS_TOKEN/${accessToken}/g" $JMX_FILE_PATH > jmx.tmp
+sed "s/ACCESS_TOKEN/${accessToken}/g" $JMX_FILE_PATH > jmx.tmp
 mv jmx.tmp $JMX_FILE_PATH
 
-sed -i "s/HOST/${host}/g" $JMX_FILE_PATH > jmx.tmp
+sed "s/HOST/${host}/g" $JMX_FILE_PATH > jmx.tmp
 mv jmx.tmp $JMX_FILE_PATH
 
 sed -i "s/API_KEY/${apiKey}/g" $JMX_FILE_PATH > jmx.tmp
