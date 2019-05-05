5ccb948fcc7674d544b1f49f1146bf09792ea9de
Manjunath Davanam <manjunathd@ilimi.in>
2019-05-05 22:50:51 +0530

    Issue #SB-11825 fix: Data generator script

--- sunbird-analytics/data-generator/kafkaDispatcher.js
+++ sunbird-analytics/data-generator/kafkaDispatcher.js
@@ -3,6 +3,7 @@ var kafka = require('kafka-node'),
     client = new kafka.KafkaClient({ kafkaHost: '28.0.3.25:9092' }),
     producer = new Producer(client);
 var count = 0;
+var incrementor = 0;
 
 client.on('ready', function() {
     console.log('kafka is ready ready');
@@ -16,7 +17,8 @@ var KafkaDispatcher = {
     dispatch: function(telemetryEvent, cb) {
         var eventObject = { "id": "loadtest.telemetry", "ver": "3.0", "ets": new Date().getTime(), "events": telemetryEvent, "mid": "56c0c430-748b-11e8-ae77-cd19397ca6b0", "syncts": 1529500243955 }
         payloads = [{
-            topic: 'loadtest.telemetry.ingest'
+            topic: 'loadtest.telemetry.ingest',
+            partition: getPartitionNumber()
         }];
         payloads[0].messages = JSON.stringify(eventObject)
         producer.send(payloads, function(err, res) {
@@ -34,4 +36,19 @@ var KafkaDispatcher = {
         });
     }
 }
+
+function getPartitionNumber() {
+    var res = undefined
+    var partition = [0, 1, 2, 3]
+    incrementor++
+    if (incrementor <= 4) {
+        res = partition[incrementor - 1]
+        if (incrementor === 4) {
+            incrementor = 0
+        }
+        return res
+    }
+
+}
+
 module.exports = KafkaDispatcher
