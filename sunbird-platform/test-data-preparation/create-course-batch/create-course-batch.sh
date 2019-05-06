aa673e8a851645bd7457d7b5bb2714cb9ea6e4af
rajat <rajatgupta0889@gmail.com>
2019-05-06 20:27:34 +0530

    add userid

--- sunbird-platform/test-data-preparation/create-course-batch/create-course-batch.sh
+++ sunbird-platform/test-data-preparation/create-course-batch/create-course-batch.sh
@@ -3,9 +3,10 @@
 start(){
 	now=`date +%Y-%m-%d`
 	count=1;
-	while read  courseId type ceatedFor; do
+	while read  courseId type ceatedFor userid; do
 		count=`expr $count + 1`
-		local query="INSERT INTO sunbird.course_batch (id, courseid, createdfor, description, enrollmenttype,startdate,status, hashtagid,rootorgid) VALUES ('course_batch-$type-$count','$courseId',['$createdFor'],'Cousrse description','$type','$now',1,'course_batch-$count');"
+		local query="INSERT INTO sunbird.course_batch (id, courseid, createdfor, description, enrollmenttype,startdate,status, hashtagid,rootorgid,createdby) VALUES ('course_batch-$type-$count','$courseId',['$createdFor'],'Cousrse description','$type','$now',1,'course_batch-$count','$userid');"
+		echo $query
 		if [ $count == 0 ]
 	then
 		echo $query > $coursebatch
