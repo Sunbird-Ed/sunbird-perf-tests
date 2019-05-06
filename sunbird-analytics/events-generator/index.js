d1a62a52508eaf9012ee4f8781e754bef29c93fe
Manjunath Davanam <manjunathd@ilimi.in>
2019-05-06 20:01:35 +0530

    Issue #SB-11825 fix: Events data generator script

---
+++ sunbird-analytics/events-generator/index.js
@@ -0,0 +1,114 @@
+// Read the process parameter
+let data = require('./data');
+let faker = require('faker');
+var async = require("async");
+let eventsToBeGenerated = process.argv[2];
+let trace = process.argv[3];
+console.log("eventsToBeGenerated" + eventsToBeGenerated)
+let events = [];
+let batchSize = 200;
+let impression = process.argv[5];
+let search = process.argv[6];
+let log = process.argv[7];
+let ratio = { impression: 100, search: 60, log: 40 };
+let loops = eventsToBeGenerated / batchSize;
+var kafkaDispatcher = require('./kafkaDispatcher')
+require('events').EventEmitter.defaultMaxListeners = 10000
+let topic = process.argv[4];
+let defaultTopic = "loadtest.telemetry.ingest"
+
+
+function getEvent(type) {
+    let event = data[type];
+    if (type === "SEARCH") {
+        event.edata.filters.dialcodes = faker.random.arrayElement(data.dialCodes)
+    }
+    event.mid = "LOAD_TEST_" + process.env.machine_id + "_" + faker.random.uuid()
+    event.context.did = faker.random.arrayElement(data.dids);
+    event.context.channel = faker.random.arrayElement(data.channelIds);
+    event.object.id = faker.random.arrayElement(data.contentIds);
+    return event;
+}
+
+function generateBatch(cb) {
+    for (let i = 0; i < ratio.log; i++) {
+        events.push(JSON.parse(JSON.stringify(getEvent('LOG'))));
+    }
+    for (let i = 0; i < ratio.impression; i++) {
+        events.push(JSON.parse(JSON.stringify(getEvent('IMPRESSION'))));
+    }
+    for (let i = 0; i < ratio.search; i++) {
+        events.push(JSON.parse(JSON.stringify(getEvent('SEARCH'))));
+    }
+    if (events.length >= batchSize) {
+        let isBatch = topic === defaultTopic ? true : false
+        dispatch(events.splice(0, batchSize), isBatch,
+            function(err, res) {
+                if (err) {
+                    console.error("error occur" + err)
+                    cb(err, undefined)
+                } else {
+                    if (cb) cb(undefined, res)
+                }
+            })
+    } else {
+        if (cb) cb()
+    }
+}
+
+function dispatch(message, batch, cb) {
+    if (batch) {
+        kafkaDispatcher.dispatchBatch(message,
+            function(err, res) {
+                if (err) {
+                    console.log('error', err);
+                    cb(err, undefined)
+                } else {
+                    cb(undefined, res)
+                }
+            })
+    } else {
+        (async function loop() {
+            for (let i = 0; i < message.length; i++) {
+                await new Promise(resolve => kafkaDispatcher.dispatch(message[i], function() {
+                    resolve(cb())
+                }));
+            }
+        })();
+    }
+}
+
+
+
+function getTraceEvents() {
+    var traceEvents = require("./tracerEvents")
+    updatedTracerEvents = []
+    traceEvents.forEach(function(e) {
+        e.mid = "LOAD_TEST_" + process.env.machine_id + "_" + faker.random.uuid() + "_TRACE"
+        e.ets = new Date().getTime()
+        e.did = faker.random.uuid()
+        updatedTracerEvents.push(JSON.parse(JSON.stringify(e)))
+    })
+    return updatedTracerEvents;
+}
+
+
+(async function loop() {
+    console.log("Loops", loops)
+    for (let i = 1; i <= loops; i++) {
+        await new Promise(resolve => generateBatch(function() {
+            resolve()
+        }));
+    }
+    if (trace === "true") {
+        var tracerEvents = getTraceEvents()
+        dispatch(tracerEvents, function(err, res) {
+            if (!err) {
+                console.log(tracerEvents.length + " Tracer Events are pushed")
+                console.log("Process is done")
+            } else {
+                console.error("Error occur due to" + err)
+            }
+        })
+    }
+})();
---
+++ sunbird-analytics/events-generator/kafkaDispatcher.js
@@ -0,0 +1,78 @@
+var kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    client = new kafka.KafkaClient({ kafkaHost: '28.0.3.25:9092, 28.0.3.27:9092, 28.0.3.26:9092' }),
+    //client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'}),
+    producer = new Producer(client)
+var count = 0;
+var incrementor = 0;
+let topic = process.argv[4];
+
+client.on('ready', function() {
+    console.log('kafka is ready ready');
+
+})
+
+client.on('error', function(err) {
+    console.log('kafka is not ready : ' + err);
+})
+var KafkaDispatcher = {
+    dispatchBatch: function(telemetryEvent, cb) {
+        var eventObject = { "id": "loadtest.telemetry", "ver": "3.0", "ets": new Date().getTime(), "events": telemetryEvent, "mid": "56c0c430-748b-11e8-ae77-cd19397ca6b0", "syncts": 1529500243955 }
+        payloads = [{
+            topic: topic,
+            partition: getPartitionNumber()
+        }];
+        payloads[0].messages = JSON.stringify(eventObject)
+        console.log("payloads" + telemetryEvent.length)
+        producer.send(payloads, function(err, res) {
+            if (res) {
+                count = count + telemetryEvent.length
+                console.log("Total Events pushed: " + count)
+                if (cb) cb(undefined, res)
+            }
+        })
+        producer.on('error', function(err, data) {
+            console.log('error: ' + err);
+            if (err) {
+                if (cb) cb(err, undefined)
+            }
+        });
+    },
+    dispatch: function(event, cb) {
+        payloads = [{
+            topic: topic,
+            partition: getPartitionNumber(),
+        }];
+        payloads[0].messages = JSON.stringify(event)
+        producer.send(payloads, function(err, res) {
+            if (res) {
+                count = count + 1
+                console.log("Total Events pushed: " + count)
+                if (cb) cb(undefined, res)
+            }
+        })
+        producer.on('error', function(err, data) {
+            console.log('error: ' + err);
+            if (err) {
+                if (cb) cb(err, undefined)
+            }
+        });
+
+    }
+}
+
+function getPartitionNumber() {
+    var res = undefined
+    var partition = [0, 1, 2, 3]
+    incrementor++
+    if (incrementor <= 4) {
+        res = partition[incrementor - 1]
+        if (incrementor === 4) {
+            incrementor = 0
+        }
+        return res
+    }
+
+}
+
+module.exports = KafkaDispatcher
