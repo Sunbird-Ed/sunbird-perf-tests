2bda030b68e53baa7460be44ec36bf30b646ee4a
rajat <rajatgupta0889@gmail.com>
2019-05-06 21:04:19 +0530

    fix

--- sunbird-platform/test-data-preparation/create-course-batch/create-course-batch.sh
+++ sunbird-platform/test-data-preparation/create-course-batch/create-course-batch.sh
@@ -8,7 +8,7 @@ while read f1 f2 f3 f4
 do
 		count=`expr $count + 1`
         echo "Line is : $f1 $f2 $f3 $f4"
-        local query="INSERT INTO sunbird.course_batch (id, courseid, createdfor, description, enrollmenttype,startdate,status, hashtagid,rootorgid,createdby) VALUES ('course_batch-$f2-$count','$f1',['$f3'],'Cousrse description','$f2','$now',1,'course_batch-$count','$f4');"
+        local query="INSERT INTO sunbird.course_batch (id, courseid, createdfor, description, enrollmenttype,startdate,status, hashtagid,createdby) VALUES ('course_batch-$f2-$count','$f1',['$f3'],'Cousrse description','$f2','$now',1,'course_batch-$f2-$count','$f4');"
         echo $query
         if [ $count == 1 ]
 	then
