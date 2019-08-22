c0427226731f70d1331c4abcbde4cd5c1cece7a6
anmol2302 <anmolgupta23021997@gmail.com>
2019-08-22 08:14:58 +0530

    Remove.sh modified

--- sunbird-platform/user-create-with-course-batch-enrol/remove.sh
+++ sunbird-platform/user-create-with-course-batch-enrol/remove.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 rm *.txt
-rm *.json 
+rm *.json
+rm /Users/anmolgupta/sunbird-perf-tests/sunbird-platform/logs/user-create-with-course-batch-enrol/modifieds_t_1/logs/scenario.log
+rm /Users/anmolgupta/sunbird-perf-tests/sunbird-platform/logs/user-create-with-course-batch-enrol/modifieds_t_1/logs/output.xml 
 ./generate-test-data.sh 10 10 10 1
