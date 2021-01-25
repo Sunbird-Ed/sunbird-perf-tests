a34881ae106858f99bbdebb601aaa12a179605f9
Juthika Paul <57178572+juthipaul@users.noreply.github.com>
2021-01-25 09:25:46 +0530

    Updated jmx and run scenario file

--- sunbird-platform/user-read/run_scenario.sh
+++ sunbird-platform/user-read/run_scenario.sh
@@ -8,20 +8,23 @@ numThreads=$5
 rampupTime=$6
 ctrlLoops=$7
 apiKey=$8
-username=$9
-csvFileHost=${10}
-csvFileRequest=${11}
-userReadApi=${12}
+accessTokenUrl=$9
+userName=${10}
+csvFileHost=${11}
+csvFileRequest=${12}
+userReadApi=${13}
 
 # Generating x-authenticated-token
-accessToken=$(curl -s -X POST https://loadtest.ntp.net.in/auth/realms/sunbird/protocol/openid-connect/token  -H 'content-type: application/x-www-form-urlencoded'  --data "client_id=admin-cli&username=${username}&password=password&grant_type=password" | jq -r '.access_token') # X-AUTHENTICATED-TOKEN
+accessToken=$(curl -s -X POST https://${accessTokenUrl}/auth/realms/sunbird/protocol/openid-connect/token  -H 'content-type: application/x-www-form-urlencoded'  --data "client_id=admin-cli&username=${userName}&password=password&grant_type=password" | jq -r '.access_token') # X-AUTHENTICATED-TOKEN
 
+echo "accessTokenUrl = " ${accessTokenUrl}
+echo "userName = " ${userName}
 echo "accessToken = " ${accessToken}
 
 JMETER_HOME=/mnt/data/benchmark/apache-jmeter-4.0
 JMETER_HOME=${jmeterHome}
 
-SCENARIO_LOGS=~/sunbird-perf-tests/sunbird-platform/logs/$scenario_name
+SCENARIO_LOGS=/mount/data/benchmark/sunbird-perf-tests/sunbird-platform/logs/$scenario_name
 
 JMETER_CLUSTER_IPS=$ips
 
@@ -40,7 +43,7 @@ mkdir $SCENARIO_LOGS/$scenario_id/logs
 mkdir $SCENARIO_LOGS/$scenario_id/server/
 
 rm ~/current_scenario/*.jmx
-cp ~/sunbird-perf-tests/sunbird-platform/$scenario_name/$scenario_name.jmx $JMX_FILE_PATH
+cp /mount/data/benchmark/sunbird-perf-tests/sunbird-platform/$scenario_name/$scenario_name.jmx $JMX_FILE_PATH
 
 echo "ip = " ${ips}
 echo "scenario_name = " ${scenario_name}
@@ -49,6 +52,8 @@ echo "numThreads = " ${numThreads}
 echo "rampupTime = " ${rampupTime}
 echo "ctrlLoops = " ${ctrlLoops}
 echo "apiKey = " ${apiKey}
+echo "accessTokenUrl = " ${accessTokenUrl}
+echo "userName = " ${userName}
 echo "accessToken = " ${accessToken}
 echo "csvFileHost = " ${csvFileHost}
 echo "csvFileRequest = " ${csvFileRequest}
@@ -85,9 +90,20 @@ mv jmx.tmp $JMX_FILE_PATH
 echo "Running ... "
 echo "$JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R ${ips} -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log"
 
-nohup $JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R ${ips} -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log 2>&1 &
+#nohup $JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R ${ips} -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log > $SCENARIO_LOGS/$scenario_id/logs/scenario.log 2>&1 &
+
+### Create HTML reports for every run ###
+$JMETER_HOME/bin/jmeter.sh -n -t $JMX_FILE_PATH -R ${ips} -l $SCENARIO_LOGS/$scenario_id/logs/output.xml -e -o $SCENARIO_LOGS/$scenario_id/logs/summary -j $SCENARIO_LOGS/$scenario_id/logs/jmeter.log | tee $SCENARIO_LOGS/$scenario_id/logs/scenario.log
+
+
+echo  "Thread Count = " ${numThreads} >> $SCENARIO_LOGS/$scenario_id/logs/summary/index.html
+echo  " Loop Count = " ${ctrlLoops} >> $SCENARIO_LOGS/$scenario_id/logs/summary/index.html
+
+az storage blob upload-batch --account-name dikshaloadtest --sas-token "?sv=2019-10-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-06-25T16:54:20Z&st=2020-06-25T08:54:20Z&spr=https&sig=%2FUKHuzhyt5sPixIXkY1jtjabC7aqV8awEXmBtKTCIOY%3D" --destination jmeter-html-reports/$scenario_id --source $SCENARIO_LOGS/$scenario_id/logs/summary | grep index.html
 
 echo "Log file ..."
 echo "$SCENARIO_LOGS/$scenario_id/logs/scenario.log"
 
 echo "Execution of $scenario_id Complete."
+
+tail -f $SCENARIO_LOGS/$scenario_id/logs/scenario.log
